566a1a9c4d53d1b325914011fb6c72cdc4952759
unknown <umeshbs037@gmail.com>
2022-09-01 17:51:17 +0530

    auth res modified

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,18 +3,28 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/application");
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
-  }
-  try {
-    const decoded = jwt.verify(token, config.secret_key);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
+    if (!token) {
+        return res.send({
+            status: 403,
+            success: false,
+            message: "A token is required for authentication"
+        });
+        // return res.status(403).send("A token is required for authentication");
+    }
+    try {
+        const decoded = jwt.verify(token, config.secret_key);
+        req.user = decoded;
+    } catch (err) {
+        return res.send({
+            status: 401,
+            success: false,
+            message: "Invalid Token"
+        });
+        // return res.status(401).send("Invalid Token");
+    }
+    return next();
 };
 
 module.exports = verifyToken;
